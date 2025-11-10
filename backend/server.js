304b52e58965d9fdee2805dee751fea8bda47032
Irungusam <irungusam8@gmail.com>
2025-11-10 18:38:06 +0300

    add User model and set up inngest for clerk

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 
-await connectDB()
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => res.send("Server is running"));
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
