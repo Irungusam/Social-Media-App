841f40583ca976e679ccbc1db37e6e758d9b3164
Irungusam <irungusam8@gmail.com>
2025-11-28 11:36:50 +0300

    add auth middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import connectDB from "./configs/db.js";
 import { inngest, functions } from "./inngest/index.js";
 import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => res.send("Server is running"));
