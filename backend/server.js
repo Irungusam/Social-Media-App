6e30e74831633819b5f711c017b2296d7435523c
Irungusam <irungusam8@gmail.com>
2026-02-18 00:28:13 +0300

    add storyRouter

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
 import userRouter from "./routes/userRoutes.js";
 import postRouter from "./routes/postRoutes.js";
+import storyRouter from "./routes/storyRoutes.js";
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use('/api/user', userRouter)
 app.use('api/post', postRouter)
+app.use('api/story', storyRouter)
 
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
